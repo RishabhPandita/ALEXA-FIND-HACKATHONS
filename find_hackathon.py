154f9410c5a2f21320cfc0e8abf0ad478a5178d2
Rishabh Pandita <rishabh.pandita@gmail.com>
2018-03-03 16:31:06 -0500

    Clean Repo

--- find_hackathon.py
+++ find_hackathon.py
@@ -6,21 +6,18 @@ from flask_ask import Ask, statement, question, session
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
-db = MySQLdb.connect(host="webscrapper-instance.c2ay3hczfmtj.us-east-2.rds.amazonaws.com", 
-			user="admin_webscrap", 
-			passwd="password", 
-			db="mlh_data")
+#db = MySQLdb.connect(host="webscrapper-instance.c2ay3hczfmtj.us-east-2.rds.amazonaws.com", 
+#			user="admin_webscrap", 
+#			passwd="password", 
+#			db="mlh_data")
 
-cur = db.cursor()
+#cur = db.cursor()
 
 
 @ask.launch
 def start_app():
-
     welcome_msg = render_template('welcome')
-    #reprompt = render_template('reprompt')
-    return question(welcome_msg).reprompt(render_template('ready'))
-
+    return question(welcome_msg)
 
 
 if __name__ == '__main__':
