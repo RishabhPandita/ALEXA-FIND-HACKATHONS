ca6fefd8aed5b98c4a41da76e064959af59c17d1
Rishabh Pandita <rishabh.pandita@gmail.com>
2018-03-03 17:04:02 -0500

    SQL State Query

--- find_hackathon.py
+++ find_hackathon.py
@@ -6,22 +6,25 @@ from flask_ask import Ask, statement, question, session
 app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger("flask_ask").setLevel(logging.DEBUG)
-#db = MySQLdb.connect(host="webscrapper-instance.c2ay3hczfmtj.us-east-2.rds.amazonaws.com", 
-#			user="admin_webscrap", 
-#			passwd="password", 
-#			db="mlh_data")
+db = MySQLdb.connect(host="webscrapper-instance.c2ay3hczfmtj.us-east-2.rds.amazonaws.com", 
+			user="admin_webscrap", 
+			passwd="password", 
+			db="mlh_data")
 
-#cur = db.cursor()
+cur = db.cursor()
 
 
 @ask.intent("PlaceIntent")
-def find_hackathons_Place(city=None,state=None,test):
-	print(city,state,test)
+def find_hackathons_Place(city=None,state=None):
+	print(city,state)
+	res=None
 	if city==None and state==None:
 		answer=render_template('NolocationPlaceHolder')
 		return statement(answer)
 	elif city ==None and state!=None:
 		city=state
+		res=cur.execute("Select Name from hackathon where state=%s",(state))
+		print(res)
 	elif state==None and city!=None:
 		pass
 	elif state !=None and city!=None:
