9986722181cf86f9a14ab8f5bd68168b447958c5
Rishabh Pandita <rishabh.pandita@gmail.com>
2018-03-03 17:12:54 -0500

    debug

--- find_hackathon.py
+++ find_hackathon.py
@@ -24,7 +24,7 @@ def find_hackathons_Place(city=None,state=None):
 	elif city ==None and state!=None:
 		city=state
 		res=cur.execute("Select Name from hackathon where state=%s",(state,))
-		print(res)
+		print("RESULT ***",res)
 	elif state==None and city!=None:
 		pass
 	elif state !=None and city!=None:
