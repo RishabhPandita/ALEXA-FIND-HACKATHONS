2fb9e9ed3ea7da2b3ebf19dbc977be6391bed801
Rishabh Pandita <rishabh.pandita@gmail.com>
2018-03-04 00:02:47 -0500

    corner cases

--- find_hackathon.py
+++ find_hackathon.py
@@ -25,13 +25,17 @@ def find_hackathons_Place(city=None,state=None):
 	elif city ==None and state!=None:
 		city=state
 		res=cur.execute("Select name,startDate,endDate from hackathon where state=%s",(state,))
-		data = cur.fetchall()
-		for row in data:
-			resString=resString+str(row[0])+" from "+str(row[1])+" to "+str(row[2])+","
-		resString = city + " , "+resString
-		answer=render_template('locationPlaceHolder',city=resString)
-		print(resString)
-		return statement(answer)
+		if res >0:
+			data = cur.fetchall()
+			for row in data:
+				resString=resString+str(row[0])+" from "+str(row[1])+" to "+str(row[2])+","
+			resString = city + " , "+resString
+			answer=render_template('locationPlaceHolder',city=resString)
+			print(resString)
+			return statement(answer)
+		elif res ==0:
+			answer=render_template('noAnswer')
+	                return statement(answer)
 	elif state==None and city!=None:
 		res=cur.execute("Select name,startDate,endDate from hackathon where city=%s",(city,))
 		data = cur.fetchall()
